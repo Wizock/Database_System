a22124033e9fdf433932db268f66000967d195c8
Ishchild ApartmentAngels <36351054+Wizock@users.noreply.github.com>
2022-04-18 01:19:55 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from api import *
 from login import *
-from session import * 
+from session import *
 import sqlite3
 
+
 def main():
-    
+
     permission = checkIfEmpty()
 
     if permission == True:
@@ -13,20 +14,20 @@ def main():
         admin_register()
 
     if permission == False:
-        con = sqlite3.connect('main_Database.db')
+        con = sqlite3.connect("main_Database.db")
         createTableFormat()
         admin_register()
-        
+
     adminOrNot = input("are you a worker or a admin?| type 'admin' or 'worker': ")
 
     if adminOrNot == "admin":
-        
+
         print("please Login:\n")
         login_attemp_admin()
-    if adminOrNot == 'worker':
+    if adminOrNot == "worker":
         print("please Login:\n")
-        login_attemp_workers()  
-if __name__=="__main__":
+        login_attemp_workers()
+
+
+if __name__ == "__main__":
     main()
-
-
