0bb54300f921845d11f7bf13f5623200fbe9cb3c
Ishchild ApartmentAngels <36351054+Wizock@users.noreply.github.com>
2022-04-18 01:20:22 +1000

    Update api.py

--- api.py
+++ api.py
@@ -2,30 +2,38 @@ import sqlite3
 from sqlite3 import Error
 import os
 
+
 def admin_register():
     con = connect()
     try:
-        con.execute('''
+        con.execute(
+            """
             INSERT INTO Admin (email,userName,password) VALUES (?,?,?)
-        ''',("Wizock.Admin@mail.com","admin","admin"))
+        """,
+            ("Wizock.Admin@mail.com", "admin", "admin"),
+        )
         con.commit()
         con.close()
         print("your Admin account has been created :)")
     except Error as e:
         print(e)
 
+
 def createTableFormat():
     conn1 = connect()
     try:
-        conn1.execute('''
+        conn1.execute(
+            """
             CREATE TABLE Admin (
                 UserId INTEGER PRIMARY KEY,
                 email varchar(255),
                 userName varchar(255),
                 password varchar(255)
                 )
-                ''')
-        conn1.execute('''
+                """
+        )
+        conn1.execute(
+            """
             CREATE TABLE employee_personal (
                 UserId INTEGER PRIMARY KEY,
                 firstName varchar(255),
@@ -38,9 +46,10 @@ def createTableFormat():
                 adress varchar(255),
                 phonenumber int
                 )
-            ''')
+            """
+        )
         conn1.execute(
-            '''
+            """
             CREATE TABLE employee_shifts(
 
                 UserId int,
@@ -51,9 +60,10 @@ def createTableFormat():
                 FOREIGN KEY (UserId) REFERENCES employee_personal(UserId),
                 FOREIGN KEY (Positions) REFERENCES employee_personal(positions)
                 )
-            ''')
+            """
+        )
         conn1.execute(
-            '''
+            """
             CREATE TABLE employee_pay(
                 UserId int,
                 Username varchar(255),
@@ -63,10 +73,10 @@ def createTableFormat():
                 FOREIGN KEY (UserId) REFERENCES employee_personal(UserId),
                 FOREIGN KEY (age) REFERENCES employee_personal(age)
             )
-            '''
+            """
         )
         conn1.execute(
-            '''
+            """
             CREATE TABLE late_arrivals(
                 UserId int,
                 employeeName varchar(255),
@@ -76,11 +86,15 @@ def createTableFormat():
                 FOREIGN KEY (UserId) REFERENCES employee_personal(UserId),
                 FOREIGN KEY (employeeName) REFERENCES employee_personal(firstName)
             )
-            '''
+            """
         )
         # this is a query which creates a table, this specific query sets up the table's format
-        check_Admin = conn1.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Admin'")
-        check_Workers = conn1.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='workers'")
+        check_Admin = conn1.execute(
+            "SELECT name FROM sqlite_master WHERE type='table' AND name='Admin'"
+        )
+        check_Workers = conn1.execute(
+            "SELECT name FROM sqlite_master WHERE type='table' AND name='workers'"
+        )
 
         if check_Admin != None:
             conn1.execute("DROP TABLE Admin")
@@ -89,15 +103,17 @@ def createTableFormat():
     except Error as e:
         print(e)
 
+
 def connect():
-    Path = os.getcwd()+"\main_DataBase.db"
+    Path = os.getcwd() + "\main_DataBase.db"
     if Path != os.path.islink(Path):
         con = sqlite3.connect(Path)
 
     return con
 
+
 def checkIfEmpty():
-    Path = os.getcwd()+"\main_DataBase.db"
+    Path = os.getcwd() + "\main_DataBase.db"
     if os.path.exists(Path) == True:
         return False
     else:
