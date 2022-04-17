7e6ce45499903a4328ec1a7b550f78f93e932295
Ishchild ApartmentAngels <36351054+Wizock@users.noreply.github.com>
2022-04-18 01:19:41 +1000

    Update session.py

--- session.py
+++ session.py
@@ -3,8 +3,9 @@ import string
 from api import *
 from main import main
 
-positions = ["grills","front","assembly","drive_through","cleaning"]
+positions = ["grills", "front", "assembly", "drive_through", "cleaning"]
 connection = connect()
+
 
 def beginAdmin():
     regOrDel = input("would you like to edit or delete a employee:")
@@ -13,16 +14,29 @@ def beginAdmin():
     if regOrDel == "delete":
         delete()
 
+
 def userName():
-    for i in range(0,3):
-        userName = str(register_firstName[i]+str(random.randint(0,999))+str(random.randint(0,999))+str(random.randint(0,999)))
+    for i in range(0, 3):
+        userName = str(
+            register_firstName[i]
+            + str(random.randint(0, 999))
+            + str(random.randint(0, 999))
+            + str(random.randint(0, 999))
+        )
     return userName
+
 
 def password():
     # Random string with the combination of lower and upper case
     letters = string.ascii_letters
-    result_str = ''.join(random.choice(letters) for i in range(3))+str(random.randint(0,3))+str(random.randint(0,999))+str(random.randint(0,999))
+    result_str = (
+        "".join(random.choice(letters) for i in range(3))
+        + str(random.randint(0, 3))
+        + str(random.randint(0, 999))
+        + str(random.randint(0, 999))
+    )
     return result_str
+
 
 def calculateWage():
     payingFactor = 20
@@ -31,7 +45,7 @@ def calculateWage():
     positionValue = 10
     position = list(register_position)
     for Positions in positions:
-        for i in range(0,len(Positions)):
+        for i in range(0, len(Positions)):
             if register_position == positions[0]:
                 positionValue += 25
             elif register_position == positions[1]:
@@ -43,30 +57,30 @@ def calculateWage():
             elif register_position == positions[4]:
                 positionValue += 26
 
-    wage = int(age*payingFactor/workingTime+positionValue)
+    wage = int(age * payingFactor / workingTime + positionValue)
 
     return wage
+
 
 def delete():
     employees = list(connection.execute("SELECT firstName FROM workers"))
     position = list(connection.execute("SELECT position FROM workers"))
     wage = list(connection.execute("SELECT wage FROM workers"))
     employeeID = list(connection.execute("SELECT employeeID FROM workers"))
-    for i in range(0,len(employees)):
-        print(employeeID[i]+":"+employees[i]+":"+position[i]+":"+wage[i])
+    for i in range(0, len(employees)):
+        print(employeeID[i] + ":" + employees[i] + ":" + position[i] + ":" + wage[i])
     employeeToDel = int(input("Which employee would you like to delete? :"))
-    connection.execute("DELETE ? FROM workers"),(employees[employeeToDel])
-
+    connection.execute("DELETE ? FROM workers"), (employees[employeeToDel])
 
 
 def edit():
-    global register_firstName, register_lastName, register_position,register_age,register_position
+    global register_firstName, register_lastName, register_position, register_age, register_position
 
     register_firstName = input("enter Employee's First Name:")
     register_lastName = input("enter Employee's Last Name:")
     register_email = input("Enter Employee's email:")
     # make User name primary on both tables
-    register_userName  = userName()
+    register_userName = userName()
     register_password = password()
     register_age = int(input("Enter Employee's age:"))
     register_position = input("Enter the assigned Employee's position:")
@@ -75,27 +89,51 @@ def edit():
         register_position = input("Enter the assigned Employee's position:")
     register_Wage = calculateWage()
 
-    registerNother = input("would you like to register another worker? | type: 'yes' or 'no' :")
+    registerNother = input(
+        "would you like to register another worker? | type: 'yes' or 'no' :"
+    )
     if registerNother == "yes":
-        connection.execute('''
+        connection.execute(
+            """
         INSERT INTO employee_personal (firstName,lastName,email,userName,password,Age,Wage,position)
         VALUES (?,?,?,?,?,?,?,?)
-        ''',
-        (register_firstName,register_lastName,register_email,register_userName,register_password,register_age,register_Wage,register_position))
+        """,
+            (
+                register_firstName,
+                register_lastName,
+                register_email,
+                register_userName,
+                register_password,
+                register_age,
+                register_Wage,
+                register_position,
+            ),
+        )
         connection.commit()
         edit()
-    if registerNother == 'no':
-        connection.execute('''
+    if registerNother == "no":
+        connection.execute(
+            """
         INSERT INTO employee_personal (firstName,lastName,email,userName,password,Age,Wage,position)
         VALUES (?,?,?,?,?,?,?,?)
-        ''',
-        (register_firstName,register_lastName,register_email,register_userName,register_password,register_age,register_Wage,register_position))
+        """,
+            (
+                register_firstName,
+                register_lastName,
+                register_email,
+                register_userName,
+                register_password,
+                register_age,
+                register_Wage,
+                register_position,
+            ),
+        )
         connection.commit()
         connection.close()
     again = input("would you like to logout? :")
     if again == "yes":
         main()
-    elif again == 'no':
+    elif again == "no":
         beginAdmin()
 
 
@@ -103,18 +141,23 @@ class Manager_session:
     def __init__(self):
         self.connection = connect()
         self.SessionTime = clock()
+
     def begin(self):
         while self.SessionTime < 25:
             pass
+
     def render_session(self):
         pass
+
 
 class Worker_session:
     def __init__(self):
         self.connection = connect()
         self.SessionTime = clock()
+
     def begin(self):
         while self.SessionTime < 25:
             pass
+
     def render_session(self):
         pass
